355e1d2a2117be73f522fc0c1ba3e2db1c5224d8
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-06-11 20:10:05 -0300

    token access

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/status', function () {
     return response()->json(
@@ -15,3 +16,6 @@ Route::get('/status', function () {
 });
 
 Route::apiResource('clients', ClientController::class);
+
+// auth routes
+Route::post('/login',[AuthController::class, 'login']);
