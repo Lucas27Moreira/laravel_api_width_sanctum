97bb6e716249285cf50cc09f2f5f8f19daf97305
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-06-13 09:48:23 -0300

    sanctum protection

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,9 @@ use App\Services\ApiRespose;
 
 Route::get('/status', function () {
     return ApiRespose::success('API is working');
-});
+})->middleware('auth:sanctum');
 
-Route::apiResource('clients', ClientController::class);
+Route::apiResource('clients', ClientController::class)->middleware('auth:sanctum');
 
 // auth routes
 Route::post('/login',[AuthController::class, 'login']);
